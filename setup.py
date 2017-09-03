0a80dd6a9fd34261452c1bfcefc19eb1932fde83
Adam Chidlow <achidlow@users.noreply.github.com>
2017-09-03 18:44:01 +0800

    Make package pip installable.

--- setup.py
+++ setup.py
@@ -1,8 +1,21 @@
 from distutils.core import setup
-from Cython.Build import cythonize
-import numpy
+
+try:
+    from Cython.Build import cythonize
+    import numpy
+except ImportError:
+    raise RuntimeError('Unsatisfied setup dependencies, please install cython and numpy first')
+
 
 setup(
-    ext_modules=cythonize("*.pyx", language="c++"),
+    name='mdlp-discretization',
+    version='0.1',
+    description='Discretization of continuous features using Fayyad\'s MDLP stop criterion.',
+    author='Henry Lin',
+    url='https://github.com/hlin117/mdlp-discretization',
+    install_requires=['numpy', 'scikit-learn', 'scipy'],
+    setup_requires=['cython', 'numpy'],
+    py_modules=['discretization'],
+    ext_modules=cythonize('*.pyx', language='c++'),
     include_dirs=[numpy.get_include()]
 )
