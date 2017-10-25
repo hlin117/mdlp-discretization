3965b4ed128fe485573e95fad49eb9bdc1884956
Joel Dodge <dodgejoel@gmail.com>
2017-10-24 21:27:05 -0700

    packaging changes (#25)
    * packaging changes
    
    * add in cpp file
    
    * general cleanup
    
    1. make install link in readme explicit
    2. link to paper links to higher quality scan
    3. setup.py is executable
    4. set version number correctly

--- README.md
+++ README.md
@@ -4,50 +4,49 @@ This is an implementation of Usama Fayyad's entropy based
 expert binning method.
 
 Please read the original paper
-<a href="http://sci2s.ugr.es/keel/pdf/algorithm/congreso/fayyad1993.pdf">here</a>
+<a href="http://web.donga.ac.kr/kjunwoo/files/Multi%20interval%20discretization%20of%20continuous%20valued%20attributes%20for%20classification%20learning.pdf">here</a>
 for more information.
 
 # Installation and Usage
 
-This code is built using Cython, so you have to run the makefile
-in the directory.  First install the requirements with
+Install using pip
 
 ```
-pip install -r requirements.txt
+pip install git+https://github.com/hlin117/mdlp-discretization
 ```
 
-and then run
+As with all python packages, it is recommended to create a virtual environment
+when using this project.
+
+# Example
 
 ```
-make
-```
-
-Afterwards, assuming that `discretization.py` and `_mdlp.so` are in the
-same directory, you can import the MDLP class.
-
-```
->>> from discretization import MDLP
+>>> from mdlp.discretization import MDLP
 >>> from sklearn.datasets import load_iris
+>>> transformer = MDLP()
 >>> iris = load_iris()
->>> X = iris.data
->>> y = iris.target
->>> mdlp = MDLP()
->>> conv_X = mdlp.fit_transform(X, y)
+>>> X, y = iris.data, iris.target
+>>> X_disc = transformer.fit_transform(X, y)
 ```
-
-I recommend creating a virtual environment for this project.
 
 # Tests
 
-To run the unit tests, first install test requirements with
+To run the unit tests, clone the repo and install in development mode
 
 ```
-pip install -r requirements-test.txt
+git clone https://github.com/hlin117/mdlp-discretization
+cd mdlp-discretization
+pip install -e .
 ```
 
-and then run
+then run tests with py.test
 
 ```
-py.test tests/
+py.test tests
 ```
 
+# Development
+
+To submit changes to this project, make sure that you have Cython installed and
+submit the compiled *.cpp file along with changes to python code after running
+installation locally.
--- setup.py
+++ setup.py
@@ -1,8 +1,62 @@
-from distutils.core import setup
-from Cython.Build import cythonize
-import numpy
+#!/usr/bin/python
+"""
+Implements the MDLP discretization criterion from Usama Fayyad's paper
+"Multi-Interval Discretization of Continuous-Valued Attributes for
+Classification Learning."
+"""
 
-setup(
-    ext_modules=cythonize("*.pyx", language="c++"),
-    include_dirs=[numpy.get_include()]
-)
+from setuptools import Extension, find_packages, setup
+
+if __name__ == '__main__':
+    # see https://stackoverflow.com/a/42163080 for the approach to pushing
+    # numpy and cython dependencies into extension building only
+    try:
+        # if Cython is available, we will rebuild from the pyx file directly
+        from Cython.Distutils import build_ext
+        sources = ['mdlp/_mdlp.pyx']
+    except:
+        # else we build from the cpp file included in the distribution
+        from setuptools.command.build_ext import build_ext
+        sources = ['mdlp/_mdlp.cpp']
+
+    class CustomBuildExt(build_ext):
+        """Custom build_ext class to defer numpy imports until needed.
+
+        Overrides the run command for building an extension and adds in numpy
+        include dirs to the extension build. Doing this at extension build time
+        allows us to avoid requiring that numpy be pre-installed before
+        executing this setup script.
+        """
+
+        def run(self):
+            import numpy
+            self.include_dirs.append(numpy.get_include())
+            build_ext.run(self)
+
+    cpp_ext = Extension(
+        'mdlp._mdlp',
+        sources=sources,
+        libraries=[],
+        include_dirs=[],
+        language='c++',
+    )
+
+    setup(
+        name='mdlp-discretization',
+        version='0.3',
+        description=__doc__,
+        license='BSD 3 Clause',
+        url='github.com/hlin117/mdlp-discretization',
+        author='Henry Lin',
+        author_email='hlin117@gmail.com',
+        install_requires=[
+            'setuptools>=18.0',
+            'numpy>=1.11.2',
+            'scipy>=0.18.1',
+            'scikit-learn>=0.18.1',
+            'pytest>=3.2.2',
+        ],
+        packages=find_packages(),
+        ext_modules=[cpp_ext],
+        cmdclass={'build_ext': CustomBuildExt},
+    )
