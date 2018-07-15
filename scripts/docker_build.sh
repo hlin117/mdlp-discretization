3c0fe321675b36ff01c81d86a1c27793ef21d8b8
Ming Yao <yaoming.thu@gmail.com>
2018-07-15 22:37:44 +0800

    fix travis

--- scripts/docker_build.sh
+++ scripts/docker_build.sh
@@ -12,7 +12,7 @@ for PYBIN in /opt/python/*/bin; do
 done
 
 # Bundle external shared libraries into the wheels
-for whl in wheelhouse/*.whl; do
+for whl in dist/*.whl; do
     auditwheel repair $whl -w /code/dist/
 done
 
